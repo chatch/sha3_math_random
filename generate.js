c9cdc98c962620ba65863a5e0768d059febe48f0
Chris Hatch <tofu@redlen.tl>
2016-08-20 14:38:49 +0700

    use streams for file writes

--- generate.js
+++ generate.js
@@ -9,21 +9,28 @@
 const fs = require('fs')
 const sha3 = require('crypto-js/sha3')
 
+const NUMBER_OF_RANDS = 10000
+
 const TS = Date.now()
 const RANDOMS_FILE = `randoms.${TS}.txt`
 const HASHES_FILE = `hashes.${TS}.txt`
-const TIMES = 10000
 
 const SHA3_OPTS = {outputLength: 256}
 const ERROR_HANDLER = (err) => {
   if (err) throw err
 }
 
-for (let count = 0; count < TIMES; count++) {
+const randsStream = fs.createWriteStream(RANDOMS_FILE);
+const hashesStream = fs.createWriteStream(HASHES_FILE);
+
+for (let count = 0; count < NUMBER_OF_RANDS; count++) {
   const rand = Math.random().toString()
   const hash = sha3(rand, SHA3_OPTS).toString();
-  console.log(rand)
-  console.log(hash)
-  fs.appendFile(RANDOMS_FILE, `${rand}\n`, ERROR_HANDLER)
-  fs.appendFile(HASHES_FILE, `${hash}\n`, ERROR_HANDLER)
+  // console.log(rand)
+  // console.log(hash)
+  randsStream.write(`${rand}\n`);
+  hashesStream.write(`${hash}\n`);
 }
+
+randsStream.end()
+hashesStream.end()
